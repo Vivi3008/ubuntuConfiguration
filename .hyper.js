6129d67ecf30be6ffdc49bea314fc61af608276c
Viviane <ramosluzviviane@gmail.com>
2020-10-28 16:01:56 -0300

    add files

---
+++ .hyper.js
@@ -0,0 +1,26 @@
+module.exports = {
+    config: {
+        updateChannel: "stable",
+        fontSize: 12,
+        fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+        fontWeight: "700",
+        fontWeightBold: "bold",
+        lineHeight: 1.2,
+        letterSpacing: 0,
+        cursorColor: "rgba(248,28,229,0.8)",
+        showHamburgerMenu: "true",
+        showWindowControls: "left",
+        selectionColor: "rgba(248,28,229,0.3)",
+        padding: "0 14px",
+        shell: "/bin/zsh",
+        shellArgs: ["--login"],
+        bell: "SOUND",
+        macOptionSelectionMode: "vertical",
+        webGLRenderer: true,
+    },
+    plugins: ["hyper-custom-controls"],
+    localPlugins: ["hyper-omni"],
+    hyperMacControls: {
+        flipped: true,
+    },
+};
